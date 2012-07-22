bc1f05432ac53a324f15452b021411c93c5f2875
tomykaira <tomykaira@gmail.com>
2012-07-22 17:24:14 +0900

    sumMoney を実装

--- src/VendingMachine.hs
+++ src/VendingMachine.hs
@@ -3,6 +3,13 @@ module VendingMachine where
 data Money = Ten | Fifty | Hundred | FiveHundred | Thousand
            deriving Show
 
+moneyToInteger :: Money -> Integer
+moneyToInteger Ten         = 10
+moneyToInteger Fifty       = 50
+moneyToInteger Hundred     = 100
+moneyToInteger FiveHundred = 500
+moneyToInteger Thousand    = 1000
+
 {-| function 'insert'
 
 >>> insert [] Ten
@@ -13,3 +20,12 @@ data Money = Ten | Fifty | Hundred | FiveHundred | Thousand
 
 insert :: [Money] -> Money -> [Money]
 insert l m = m:l
+
+{-| function 'sumMoney'
+
+>>> sumMoney [Ten]
+10
+-}
+
+sumMoney :: [Money] -> Integer
+sumMoney = foldl (\s m -> s + moneyToInteger m) 0
